74791bd1781464d316e58f247cc36e79383d7bae
Rakesh Ravi Shankar <kcrakeshravi@gmail.com>
2018-04-23 17:49:21 -0400

    Data gen scripts

--- tests/test2.py
+++ tests/test2.py
@@ -2,152 +2,178 @@ import gilfullList as gfl
 import gillessList as gll
 import time
 import threading
+import gc
+import sys
 
 
 class Test():
-	def __init__(self, ltype):
+	def __init__(self, ltype, numObjects=50):
 		self.ltype = ltype
+		self.numObjects = numObjects
 
 
 	def test_create(self):	
-		print("Create list test")		
 		for i in range(50):
 			self.ltype.list_create(4)
+		
 
 	def test_setget(self):
 		
-		l = self.ltype.list_create(50)
+		l = self.ltype.list_create(self.numObjects)
 		
-		for i in range(50):
+		for i in range(self.numObjects):
 			self.ltype.set_item(l, i, 99)
 		
-		for i in range(50):
+		
+		for i in range(self.numObjects):
 			self.ltype.get_item(l, i)
+		
 
 
 	def test_append(self):
 		
 		l = self.ltype.list_create(4)
-		for i in range(50):
+		for i in range(self.numObjects):
 			self.ltype.list_append(l, 99)
 		
 		
-		
-def main():
-	print("Testing gilfullList for single threaded ")
-	testgfl = Test(gfl)
-
-	#print("Create list test")
-	start = time.time()
-	testgfl.test_create()
-	end = time.time()
-	total = end - start
-	print("Time taken for create 50 lists: " + str(total))
-
-	print("In set & get test")
-	start = time.time()
-	testgfl.test_setget()
-	end = time.time()
-	total = end - start
-	print("Time taken for set and get 50 objects: " + str(total))
-
-	print("List append test")
-	start = time.time()
-	testgfl.test_append()
-	end = time.time()
-	total = end - start
-	print("Time taken for appending 50 objects: " + str(total))
-
-	print("\n\nTesting gillessList for single threaded  ")	
-	testgll = Test(gll)
-
-	print("Create list test")
-	start = time.time()
-	testgll.test_create()
-	end = time.time()
-	total = end - start
-	print("Time taken for create 50 lists: " + str(total))
-
-	print("In set & get test")
-	start = time.time()
-	testgll.test_setget()
-	end = time.time()
-	total = end - start
-	print("Time taken for set and get 50 objects: " + str(total))
-
-	print("List append test")
-	start = time.time()
-	testgll.test_append()
-	end = time.time()
-	total = end - start
-	print("Time taken for appending 50 objects: " + str(total))
-
-
-	print("\n\nTesting gilfullList for multi threaded ")
-	testgfl = Test(gfl)
 	
 
-	print("Create list test")
+def single_threaded_test(obj):
+	
 	start = time.time()
-	for i in range(3):
-		t1 = threading.Thread(target=testgfl.test_create())
-		t1.daemon=True
-		t1.start()
+	obj.test_create()
 	end = time.time()
-	total = end - start
-	print("Time taken for create 50 lists: " + str(total))
+	create_list_time = end - start
+	print("Time taken for create 50 lists: " + str(create_list_time))
 
 	print("In set & get test")
 	start = time.time()
-	for i in range(3):
-		t2 = threading.Thread(target=testgfl.test_setget())
-		t2.daemon=True
-		t2.start()
+	obj.test_setget()
 	end = time.time()
-	total = end - start
-	print("Time taken for set and get 50 objects: " + str(total))
+	set_get_time = end - start
+	print("Time taken for set and get " + str(obj.numObjects) + " objects: " + str(set_get_time))
 
 	print("List append test")
 	start = time.time()
-	for i in range(3):
-		t3 = threading.Thread(target=testgfl.test_setget())
-		t3.daemon=True
-		t3.start()
+	obj.test_append()
 	end = time.time()
-	total = end - start
-	print("Time taken for appending 50 objects: " + str(total))
-    
-	print("\n\nTesting gillessList for multi threaded  ")	
-	testgll = Test(gll)
+	append_time = end - start
+	print("Time taken for appending " + str(obj.numObjects) + " objects: " + str(append_time))
+
+	return (create_list_time, set_get_time, append_time)
+
+
+def multi_threaded_test(obj, numThreads):
 	print("Create list test")
-	start = time.time()
-	for i in range(3):
-		t1 = threading.Thread(target=testgll.test_create())
-		t1.daemon=True
-		t1.start()
-	end = time.time()
-	total = end - start
-	print("Time taken for create 50 lists: " + str(total))
+	create_list_time = 0
+	threads = {}
+	for i in range(numThreads):
+		t = threading.Thread(target=obj.test_create())
+		threads[t] = time.time()
+		t.start()
+		
+	for t in threads:
+		t.join()
+		create_list_time += (time.time() - threads[t]) / numThreads
+	print("Time taken for create 50 lists: " + str(create_list_time))
 
 	print("In set & get test")
-	start = time.time()
-	for i in range(3):
-		t2 = threading.Thread(target=testgll.test_setget())
-		t2.daemon=True
-		t2.start()
-	end = time.time()
-	total = end - start
-	print("Time taken for set and get 50 objects: " + str(total))
+	set_get_time = 0
+	threads = {}
+	for i in range(numThreads):
+		t = threading.Thread(target=obj.test_setget())
+		threads[t] = time.time()
+		t.start()
+		
+	for t in threads:
+		t.join()
+		set_get_time += (time.time() - threads[t]) / numThreads
+	print("Time taken for set and get " + str(obj.numObjects) + " objects: " + str(set_get_time))
 
 	print("List append test")
-	start = time.time()
-	for i in range(3):
-		t3 = threading.Thread(target=testgll.test_setget())
-		t3.daemon=True
-		t3.start()
-	end = time.time()
-	total = end - start
-	print("Time taken for appending 50 objects: " + str(total))
+	append_time = 0
+	threads = {}
+	for i in range(numThreads):
+		t = threading.Thread(target=obj.test_append())
+		threads[t] = time.time()
+		t.start()
+		
+	for t in threads:
+		t.join()
+		append_time += (time.time() - threads[t]) / numThreads
+	print("Time taken for appending " + str(obj.numObjects) + " objects: " + str(append_time))
+
+	return (create_list_time, set_get_time, append_time)
+		
+
+def main():
+
+	gc.disable()
+	sys.setcheckinterval(1000)
+
+	create_data_file = open("create_data.csv", "w")
+	set_get_data_file = open("set_get_data.csv", "w")
+	append_data_file = open("append_data.csv", "w")
+
+	for i in range(0, 80, 10):
+		testgfl = Test(gfl, i)
+		testgll = Test(gll, i)
+
+		gfl_create_avg = 0
+		gfl_get_set_avg = 0
+		gfl_append_avg = 0
+
+		gll_create_avg = 0
+		gll_get_set_avg = 0
+		gll_append_avg = 0
+
+		for j in range(50):
+
+	
+			# print("Testing gilfullList for single threaded ")
+			# create_list_time, set_get_time, append_time = single_threaded_test(testgfl)
+			# gfl_create_avg += create_list_time
+			# gfl_get_set_avg += create_list_time
+			# gfl_append_avg += append_time
+
+
+			# print("\n\nTesting gillessList for single threaded  ")	
+			# create_list_time, set_get_time, append_time = single_threaded_test(testgll)
+			# gll_create_avg += create_list_time
+			# gll_get_set_avg += create_list_time
+			# gll_append_avg += append_time
+
+
+			print("\n\nTesting gilfullList for multi threaded ")	
+			create_list_time, set_get_time, append_time = multi_threaded_test(testgfl, 3)
+			gfl_create_avg += create_list_time
+			gfl_get_set_avg += create_list_time
+			gfl_append_avg += append_time
+		    		
+
+			print("\n\nTesting gillessList for multi threaded  ")	
+			create_list_time, set_get_time, append_time = multi_threaded_test(testgll, 3)
+			gll_create_avg += create_list_time
+			gll_get_set_avg += create_list_time
+			gll_append_avg += append_time
+
+		gfl_create_avg /= 50
+		gfl_get_set_avg /= 50
+		gfl_append_avg /= 50
+
+		gll_create_avg /= 50
+		gll_get_set_avg /= 50
+		gll_append_avg /= 50
+
+		
+		create_data_file.write(str(i) + "," + str(gll_create_avg) + "," + str(gfl_create_avg) + "\n")
+		set_get_data_file.write(str(i) + "," + str(gll_get_set_avg) + "," + str(gfl_get_set_avg) + "\n")
+		append_data_file.write(str(i) + "," + str(gll_append_avg) + "," + str(gfl_append_avg) + "\n")
+
+	create_data_file.close()
+	set_get_data_file.close()
+	append_data_file.close()
 	
 
 
